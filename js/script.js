40d802919dbe2f90ccb973812fe2a7bf66ead36a
toddbascombe <38668582+toddbascombe@users.noreply.github.com>
2019-06-27 02:23:38 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,37 +35,8 @@ const totalPages = list => {
   return list.length / itemsPerPage;
 };
 
-//search for the letter in the students name
-function searchList(searchInput, list) {
-  let filter = searchInput.value.toLowerCase();
-  let tempList = [];
-  for (let i = 0; i < list.length; i++) {
-    list[i].classList.remove("match");
-    let h3 = list[i].getElementsByTagName("h3")[0];
-    let name = h3.textContent;
-    if (name.toLowerCase().indexOf(filter) > -1) {
-      list[i].style.display = "block";
-      tempList.push(list[i]);
-    } else {
-      list[i].style.display = "none";
-    }
-  }
-  return tempList;
-}
-
+//creates pagination links function
 function appendPageLinks(list) {
-  //create the search input and search button
-  const searchInput = document.createElement("input");
-  const searchButton = document.createElement("button");
-  searchButton.type = "submit";
-  searchButton.className = "student-search";
-  searchButton.textContent = "Search";
-  searchInput.className = "student-search";
-  searchInput.type = "search";
-  searchInput.name = "searchInput";
-  document.querySelector(".page-header").appendChild(searchInput);
-  document.querySelector(".page-header").appendChild(searchButton);
-
   //number of pages returned from totalPages()
   const numberOfPages = totalPages(list);
   //create a div element to contain the pagination links
@@ -105,48 +76,6 @@ function appendPageLinks(list) {
       if (a[i].textContent === e.target.textContent) {
         e.target.className = "active";
         showPage(list, e.target.textContent);
-      }
-    }
-  });
-
-  //listen for the key release on the key board
-  searchInput.addEventListener("keyup", () => {
-    ul_section = document.querySelector(".student-list");
-    if (searchInput.value.length <= 0) {
-      //display the default list of student
-      showPage(list, 1);
-      //handle repeated numbers in the links
-      li_dup = document.querySelectorAll(".links");
-      for (let i = 0; i < li_dup.length; i++) {
-        li_dup[i].remove();
-      }
-
-      //remove the h2 element if created on reset
-      noMatch = document.querySelectorAll(".noMatch");
-      for (let j = 0; j < noMatch.length; j++) {
-        noMatch[j].remove();
-        console.log("working");
-      }
-      //reset the pagination links
-      pageNumbers(numberOfPages);
-    } else {
-      //get the students searched
-      let info = searchList(searchInput, list);
-      let number = totalPages(info);
-      if (number === 0) {
-        //display no matches
-        const h2 = createElement("h2");
-        h2.textContent = "No match Found";
-        h2.className = "noMatch";
-        ul_section.appendChild(h2);
-      } else {
-        //remove default links
-        li = document.querySelectorAll(".links");
-        for (let i = 0; i < li.length; i++) {
-          li[i].remove();
-        }
-        //add new links, in result of the students displayed
-        pageNumbers(number);
       }
     }
   });
